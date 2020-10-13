cdab7fcac618ed64c854d61e70ec908081c362d1
tal mekler <tal.mekler11@gmail.com>
2020-10-13 20:39:10 +0300

    Update some_codes_jsCode.js

--- some_codes_jsCode.js
+++ some_codes_jsCode.js
@@ -51,3 +51,19 @@ $(function () {
         // parallaxEffect(".contact-page");
     });
 })
+
+// Read from JSON
+$.ajax({
+    dataType: "json",
+    url: "/cmds.json",
+  }).done(function (resp) {
+    for (var p in resp) {
+      if (!resp.hasOwnProperty(p)) continue;
+      var writer = resp[p].writer;
+      var command = resp[p].command;
+      cmd_wrapper.html(cmd_wrapper.html() + "<div class='cmd'>\
+        <h3>" + writer + "</h3>\
+        <p>"+ command +"</p>\
+      </div>");
+      }
+  });
